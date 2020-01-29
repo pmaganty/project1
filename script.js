9de8ca4efb48e6ff126b09f0634c8ebf720b367b
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-01-28 19:40:14 -0600

    Added code from postman to JavaScript.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-$( document ).ready()
+$( document ).ready();
+
+// var settings = {
+//     "url": "https://api.petfinder.com/v2/oauth2/token",
+//     "method": "POST",
+//     "timeout": 0,
+//     "headers": {
+//       "Content-Type": "application/x-www-form-urlencoded"
+//     },
+//     "data": {
+//       "grant_type": "client_credentials",
+//       "client_id": "UcVkPk5GZZCz7fJitqdwfUuB3XyKtgfU9WasE0dqf1DUeMeWCW",
+//       "client_secret": "WRXLteVINvKRhFlMA4eCPfFzXvYp0bHBNM07IZOY"
+//     }
+//   };
+  
+//   $.ajax(settings).done(function (response) {
+//     console.log(response);
+//   });
+
+// var settings = {
+//     "url": "https://api.petfinder.com/v2/animals?type=dog&page=2&location=78758",
+//     "method": "GET",
+//     "timeout": 0,
+//     "headers": {
+//       "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQzMzE2ZGNiNWM1NzQ0ZWRmODg1MzhkYmJkMzU1MGI3ZGE3YTE5ZTI5ZWIyM2UyMmMxMmM2Nzk4MGJiNWRlYWE5MDhkZDdmMmMwY2M5MjMwIn0.eyJhdWQiOiJVY1ZrUGs1R1paQ3o3ZkppdHFkd2ZVdUIzWHlLdGdmVTlXYXNFMGRxZjFEVWVNZVdDVyIsImp0aSI6ImQzMzE2ZGNiNWM1NzQ0ZWRmODg1MzhkYmJkMzU1MGI3ZGE3YTE5ZTI5ZWIyM2UyMmMxMmM2Nzk4MGJiNWRlYWE5MDhkZDdmMmMwY2M5MjMwIiwiaWF0IjoxNTgwMjYwMjAyLCJuYmYiOjE1ODAyNjAyMDIsImV4cCI6MTU4MDI2MzgwMiwic3ViIjoiIiwic2NvcGVzIjpbXX0.OGifD4ZA-Ekyadj5hfopWixpqsQsTF1rmUrRlB2-cbrt6DjfR0nl7I3uQM3LciMdbry-cLrCb-U_q5dUiUWRywjyNxL60nZ7ndzNDe_7PptqXoFhBdKW0fH-WlujhazEtndiLrLKDN9P8FuhNPmJq9YssiXGLNKGS1_1RxZzONNyaEhwwpyUFkprLMxdH4ZwDhS0eqlbwBE42slVDRSvWjLfmHm8K6ycmwvJiF_cK8CZj6ADP3fMMZbmD8JobPilZJah2rBGXxOD4NOGdwnEYTeecN7aQEkX0XtUIEwr0irlQRqeuuHOzfqi0pad7WiOAzvDWuEYzXuJVydiL31GxA"
+//     },
+//   };
+  
+//   $.ajax(settings).done(function (response) {
+//     console.log(response);
+//   });
